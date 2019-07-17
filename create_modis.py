88c6b212f6068103aa89259b88fdf071cb2f7568
Richard Strange <richardpstrange@outlook.com>
2019-07-17 17:27:29 +0100

    tiles extracting and pipeline is end-to-end
    a tile extraction tools has been added, and a notebook and a few test scripts added to the directory.

--- create_modis.py
+++ create_modis.py
@@ -1,11 +1,6 @@
-#!usr/bin/env python
-from satpy import Scene, available_readers
-from satpy.writers import to_image, get_enhanced_image
-from PIL import Image, ImageDraw
+from satpy import Scene
 import glob
 import numpy as np
-import sys
-from netCDF4 import Dataset
 import image_slicer
 import pickle
 
@@ -27,11 +22,11 @@ def run(path, save_dir='./test_dump/'):
 			pickle.dump(dump_array, f)
 
 
-# get the filenames for the radiances and the geolocation information
 def get_modis_filenames(path):
 	"""
-	:param path:
-	:return:
+	:param path: Path to the directory of MOD02 files
+	:return: The filenames for the matched counterpart files
+	get the filenames for the radiances and the geolocation information
 	"""
 	radiance_filenames = glob.glob(path + '*D021KM*.hdf')
 	matches = []
@@ -44,38 +39,32 @@ def get_modis_filenames(path):
 
 
 def get_arrays(files):
+	"""
+	:param files: list of nested paired MOD02 and MOD03 files
+	:return: numpy array of nested bands per MODIS02 file
+	Uses the satpy Scene reader with the modis-l1b files. Issues reading files might be due to pyhdf not being
+	installed - otherwise try pip install satpy[modis0l1b]
+	"""
 	composite = ['1', '2', '29', '33', '34', '35', '36', '26', '27', '20', '21', '22', '23']
 	# load the global scene using satpy
 	# expects filenames to be radiance then geoloc
 	# filenames must be in specific format, chop off beginning to work
-	global_scene = Scene(reader = 'modis_l1b', filenames = files)
-	# for future reference here are all available datasets
-	available_datasets = global_scene.available_dataset_names()
-	# want to create a cloudtop composite
-	# for future reference all available composites
-	available_composites = global_scene.available_composite_names()
+	global_scene = Scene(reader='modis_l1b', filenames=files)
 
 	# print available_composites
 	# expects the composite to be in a list
-	global_scene.load(composite, resolution = 1000)
+	global_scene.load(composite, resolution=1000)
 	
 	# for database structuring
-	global_scene.load(['latitude', 'longitude'], resolution = 1000)
+	global_scene.load(['latitude', 'longitude'], resolution=1000)
 	latitude = np.array(global_scene['latitude'].load())
 	longitude = np.array(global_scene['longitude'].load())
 	dump_array = []
 	# note that we only take til 1350 to avoid any bowtie effects
 	for comp in composite:
 		temp = np.array(global_scene[comp].load())
-		dump_array.append(temp[:,:1350])
-	dump_array.append(latitude[:,:1350])
-	dump_array.append(longitude[:,:1350])
-	# if you want a pic to pop up of the composite uncomment below
-	# global_scene.show(composite)
-	
+		dump_array.append(temp[:, :1350])
+	dump_array.append(latitude[:, :1350])
+	dump_array.append(longitude[:, :1350])
+
 	return np.array(dump_array)
-
-
-
-
-
