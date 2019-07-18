05ef0e458271300639a9bda3e4270b9b2a35fcce
Richard Strange <richardpstrange@outlook.com>
2019-07-18 14:36:08 +0100

    created create_modis wrapper

---
+++ create_modis_wrapper.py
@@ -0,0 +1,19 @@
+import create_modis
+import os
+
+
+def create_modis_for_dirs(source_dir, save_dir):
+    modis_paths = []
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            modis_paths.append(os.path.join(root, file))
+
+    source_dirs = set()
+    for path in modis_paths:
+        head, tail = os.path.split(path)
+        source_dirs.add(head)
+
+    for directory in source_dirs:
+        print(directory)
+
+        create_modis.run(path="{}/".format(directory), save_dir=save_dir)
