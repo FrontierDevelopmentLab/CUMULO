4b50afd0b7a4c73daf03a262276c91c49a183325
R <richardpstrange@outlook.com>
2019-07-19 12:13:34 +0000

    Added bash file walks for better path handling uses bash find . 	etc...

--- create_modis_wrapper.py
+++ create_modis_wrapper.py
@@ -4,16 +4,14 @@ import os
 
 def create_modis_for_dirs(source_dir, save_dir):
     modis_paths = []
-    for root, dirs, files in os.walk(source_dir):
-        for file in files:
-            modis_paths.append("{}/{}".format(root, file))
-    print(modis_paths)
+    bashed_files = os.popen("find ../DATA/modis-l1 -type f").read()
+    filepaths = bashed_files.split("\n")
     source_dirs = set()
-    for path in modis_paths:
+    for path in filepaths:
         head, tail = os.path.split(path)
         source_dirs.add(head)
 
     for directory in source_dirs:
         print(directory)
 
-        create_modis.run(path="{}/".format(directory), save_dir=save_dir)
+        create_modis.run(path=directory, save_dir=save_dir)
--- process_hdfs.py
+++ process_hdfs.py
@@ -2,8 +2,8 @@ import create_modis_wrapper
 
 print("source_directory: ../DATA/modis-l1/")
 
-print("save_directory: ../DATA/output/")
+print("save_directory: ../DATA/processed/")
 
-create_modis_wrapper.create_modis_for_dirs(source_dir="./DATA/modis-l1", save_dir="./DATA/processed")
+create_modis_wrapper.create_modis_for_dirs(source_dir="./DATA/modis-l1/", save_dir="./DATA/processed")
 
 print("END OPERATION: files processed")
