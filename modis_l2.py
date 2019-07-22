07d5e56f966626bd467b676f6842b2f2266f5314
Alyson Douglas <rosealyd@gmail.com>
2019-07-22 20:31:17 +0000

    Update to find the level 2 filename given a level 1 filename

--- modis_l2.py
+++ modis_l2.py
@@ -5,10 +5,18 @@ import glob
 import pickle
 import datetime
 
-def run(filename, output_dir = ''):
+def get_l2_filename(filename_example, root):
+    #MYD021KM.A2008003.1855.061.2018031033116.hdf l1
+    #MYD06_L2.A2008001.0000.061.2018031060235.hdf l2 
+    parts = filename_example.split('.')
+    l2_filename = glob.glob(root + 'MYD06_L2.{}.{}.*.hdf'.format(parts[1], parts[2]))[0]
+    return l2_filename
+
+def run(l1_filename, root_dir, output_dir = ''):
 	#output_filename = filename.split('A')[1].split('.')
 	#dt = datetime.datetime(year = int(output_filename[0][:4]), month = 1, day = 1, hour = int(output_filename[1][:2]), minute = int(output_filename[1][2:]) )
 	#dt = dt + datetime.timedelta(days = int(output_filename[0][4:])-1)
+	filename = get_l2_filename(l1_filename, root_dir)
 	level_data = SD(filename, SDC.READ)
 	latitude = level_data.select('Latitude').get()
 	longitude = level_data.select('Longitude').get()
