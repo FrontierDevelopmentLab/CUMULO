2fbec7bb7b553506c1a990f2e6e733ee81b3f9c2
Richard Strange <richardpstrange@outlook.com>
2019-07-25 00:02:47 +0100

    changes to modisl2 and pipeline scripts ready for testing

--- modis_l2.py
+++ modis_l2.py
@@ -3,31 +3,38 @@ from pyhdf.SD import SD, SDC
 import pyhdf.HDF as HDF
 import glob
 import numpy as np
+
+
 '''take in the MODIS level 1 filename to get the information needed to find the corresponding MODIS level 2 filename.
 This info includes the YYYY and day in year (ex: AYYYYDIY) and then the time of the pass (ex1855)
 It returns the full level 2 filename path'''
+
+
 def get_l2_filename(filename_example, root):
-    #MYD021KM.A2008003.1855.061.2018031033116.hdf l1
-    #MYD06_L2.A2008001.0000.061.2018031060235.hdf l2 
+    # MYD021KM.A2008003.1855.061.2018031033116.hdf l1
+    # MYD06_L2.A2008001.0000.061.2018031060235.hdf l2
     parts = filename_example.split('.')
     l2_filename = glob.glob(root + 'MYD06_L2.{}.{}.*.hdf'.format(parts[1], parts[2]))[0]
     return l2_filename
-    
+
+
 '''take in the level 1 filename and rootdir for the level 2 data, finds the cloud optical depth
 (cod), liquid water path (lwp), and cloud mask for the swath'''
+
+
 def run(l1_filename, root_dir):
-	filename = get_L2_filename(l1_filename, root_dir)
-	level_data = SD(filename, SDC.READ)
-	latitude = level_data.select('Latitude').get()
-	longitude = level_data.select('Longitude').get()
-	latitude = latitude[:][:1350]
-	longitude = longitude[:][:1350]
-	lwp = level_data.select('Cloud_Water_Path').get()[:,:1350].tolist()
-	cod = level_data.select('Cloud_Optical_Thickness').get()[:,:1350].tolist()
-	#remove fill value of -9999 and replace with np.nan
-	for i in range(len(lwp)):
-		for j in range(len(lwp[i])):
-			if lwp[i][j] == -9999:
-				cod[i][j] = float('NaN')
-				lwp[i][j] = float('NaN')
-	return np.array(lwp), np.array(cod)
+    filename = get_l2_filename(l1_filename, root_dir)
+    level_data = SD(filename, SDC.READ)
+    latitude = level_data.select('Latitude').get()
+    longitude = level_data.select('Longitude').get()
+    latitude = latitude[:][:1350]  # Not used?
+    longitude = longitude[:][:1350]  # Not used?
+    lwp = level_data.select('Cloud_Water_Path').get()[:, :1350].tolist()
+    cod = level_data.select('Cloud_Optical_Thickness').get()[:, :1350].tolist()
+    # remove fill value of -9999 and replace with np.nan
+    for i in range(len(lwp)):
+        for j in range(len(lwp[i])):
+            if lwp[i][j] == -9999:
+                cod[i][j] = float('NaN')
+                lwp[i][j] = float('NaN')
+    return np.array(lwp), np.array(cod)
