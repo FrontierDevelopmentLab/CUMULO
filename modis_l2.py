9e17e0bedabca139cb3e15b4709be4cbc352de9a
Alyson Douglas <rosealyd@gmail.com>
2019-07-24 09:27:20 +0000

    Added more comments, info on functions.

--- modis_l2.py
+++ modis_l2.py
@@ -4,14 +4,18 @@ import pyhdf.HDF as HDF
 import glob
 import pickle
 import datetime
-
+'''take in the MODIS level 1 filename to get the information needed to find the corresponding MODIS level 2 filename.
+This info includes the YYYY and day in year (ex: AYYYYDIY) and then the time of the pass (ex1855)
+It returns the full level 2 filename path'''
 def get_l2_filename(filename_example, root):
     #MYD021KM.A2008003.1855.061.2018031033116.hdf l1
     #MYD06_L2.A2008001.0000.061.2018031060235.hdf l2 
     parts = filename_example.split('.')
     l2_filename = glob.glob(root + 'MYD06_L2.{}.{}.*.hdf'.format(parts[1], parts[2]))[0]
     return l2_filename
-
+    
+'''take in the level 1 filename and rootdir for the level 2 data, finds the cloud optical depth
+(cod), liquid water path (lwp), and cloud mask for the swath'''
 def run(l1_filename, root_dir, output_dir = ''):
 	#output_filename = filename.split('A')[1].split('.')
 	#dt = datetime.datetime(year = int(output_filename[0][:4]), month = 1, day = 1, hour = int(output_filename[1][:2]), minute = int(output_filename[1][2:]) )
@@ -22,11 +26,10 @@ def run(l1_filename, root_dir, output_dir = ''):
 	longitude = level_data.select('Longitude').get()
 	latitude = latitude[:][:1350]
 	longitude = longitude[:][:1350]
-	lwp = level_data.select('Cloud_Water_Path').get()
-	lwp = lwp[:1350]
-	cod = level_data.select('Cloud_Optical_Thickness').get()
-	cod = cod[:1350]
-	cloud_mask = level_data.select('Cloud_Mask_1km').get()[:,:1350,0]
+	lwp = level_data.select('Cloud_Water_Path').get()[:][:1350]
+	cod = level_data.select('Cloud_Optical_Thickness').get()[:][:1350]
+	cloud_mask = level_data.select('Cloud_Mask_1km').get()
+	#option to easily dump this info into a pickle file if ever desired in the future
 	#with open(output_dir + output_filename, 'w') as f:
 	#	pickle.dump([lwp, cod, cloud_mask, latitude, longitude, dt], f)
 	return lwp, cod, cloud_mask
