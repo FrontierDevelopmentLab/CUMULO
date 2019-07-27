aebc296e14431eef649ede67022258330c2e2362
Valentina Zantedeschi <vzantedeschi@gmail.com>
2019-07-27 14:23:56 +0200

    ifmain for clean module imports

--- align_track.py
+++ align_track.py
@@ -2,30 +2,6 @@ import numpy as np
 import math
 import pdb
 
-test_lat = np.array([[8., 10., 12.],
-                     [8.1, 10., 12.2],
-                     [8.6, 10.9, 12.1],
-                     [9.6, 11.1, 13.1],
-                     [10.6, 11.9, 13.5]])
-
-test_lon = np.array([[10.,  20.,  30.], 
-                     [10.1, 21.1, 33.3], 
-                     [12.9, 22.9, 34.4], 
-                     [14.2, 26.1, 35.5], 
-                     [15.4, 28.9, 36.6]])
-    
-points   = np.array([[1, 8.7, 11.1], 
-                     [3, 9.1, 18.4],
-                     [7, 10.1,39.1],
-                     [6, 13.7,45.9]])
-
-# fast euclidean distance computation: https://stackoverflow.com/questions/37794849/efficient-and-precise-calculation-of-the-euclidean-distance
-def eudis5(v1, v2):
-    dist = [(a - b)**2 for a, b in zip(v1, v2)]
-    #dist = math.sqrt(sum(dist))
-    return dist
-
-
 def align(track_points, swath_lat, swath_lon):
     p = track_points.shape[0]
     n = swath_lat.shape[0]
@@ -63,4 +39,30 @@ def align(track_points, swath_lat, swath_lon):
     labels = np.argmax(np.sum(labels,axis=3),axis=2)
     return labels
 
-labels = align(points, test_lat, test_lon)
+if __name__ == "__name__":
+
+    # example
+    test_lat = np.array([[8., 10., 12.],
+                     [8.1, 10., 12.2],
+                     [8.6, 10.9, 12.1],
+                     [9.6, 11.1, 13.1],
+                     [10.6, 11.9, 13.5]])
+
+    test_lon = np.array([[10.,  20.,  30.], 
+                         [10.1, 21.1, 33.3], 
+                         [12.9, 22.9, 34.4], 
+                         [14.2, 26.1, 35.5], 
+                         [15.4, 28.9, 36.6]])
+        
+    points   = np.array([[1, 8.7, 11.1], 
+                         [3, 9.1, 18.4],
+                         [7, 10.1,39.1],
+                         [6, 13.7,45.9]])
+
+    # fast euclidean distance computation: https://stackoverflow.com/questions/37794849/efficient-and-precise-calculation-of-the-euclidean-distance
+    def eudis5(v1, v2):
+        dist = [(a - b)**2 for a, b in zip(v1, v2)]
+        #dist = math.sqrt(sum(dist))
+        return dist
+
+    labels = align(points, test_lat, test_lon)
