1d65901fc4f70324f59cac18d6319335e15e9ddf
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-07-30 16:34:43 +0000

    monkey patching

--- run_pipeline.bash
+++ run_pipeline.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find /mnt/disks/disk4/l1_aqua/ -type f | grep "MYD021KM" | xargs --max-procs=96 -n 1 python semisupervised_pipeline_sequential.py
+find ../DATA/aqua-data/level_1/2008/01/01 -type f | grep "MYD021KM" | xargs --max-procs=64 -n 1 python semisupervised_pipeline_sequential.py
