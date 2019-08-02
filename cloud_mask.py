d34098455c81a631bc635ef1b172da4a336e58b2
Alyson Douglas <rosealyd@gmail.com>
2019-08-02 15:25:17 +0000

    Removed print statement

--- cloud_mask.py
+++ cloud_mask.py
@@ -5,7 +5,6 @@ import sys
 
 def get_cloud_mask(cloud_mask_dir, level_1_filename):
     cloud_mask_filename = glob.glob(cloud_mask_dir + '*' + level_1_filename.split('.A')[1][:12] + '*')[0]
-    print cloud_mask_filename
     swath = Scene(reader = 'modis_l2', filenames = [cloud_mask_filename, level_1_filename])
     swath.load(['cloud_mask'], resolution = 1000)
     cloud_mask = np.array(swath['cloud_mask'].load())[:, :1350]
