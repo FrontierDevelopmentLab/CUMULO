1910312bc5793c9fe0405d13f444831e32fcf13e
Valentina Zantedeschi <vzantedeschi@gmail.com>
2019-08-19 19:44:36 +0200

    get rid of useless files

--- tester.py
+++
@@ -1,8 +0,0 @@
-from extract_payload import extract_labels_and_cloud_tiles
-import numpy as np
-
-file_name = "tester_string_file"
-input_swath = np.load(
-    "../cloud_fxns/results_aqua_semisuper-sequential_swath_daylight_swath_MYD021KM.A2008001.0100.061.2018031001704.npy")
-
-a, b, c, d = extract_labels_and_cloud_tiles(input_swath, file_name)
