35f041b96d959d6d11ff4da9f7eca9e4b51bc55e
Valentina Zantedeschi <vzantedeschi@gmail.com>
2019-08-21 16:25:54 +0200

    functions for extracting sampling masks

--- src/tile_extraction.py
+++ src/tile_extraction.py
@@ -2,228 +2,60 @@ import numpy as np
 import os
 import random
 
-def random_tile_extract_from_file(swath_array, file_path, tile_size=3):
+def get_sampling_mask(mask_shape=(2030, 1350), tile_size=3):
+    """ returns a mask of allowed centers for the tiles to be sampled. The center of an even size tile is considered to be the point at the position (size // 2 + 1, size // 2 + 1) within the tile.
     """
-    :param swath_array: an input numpy array of a MODIS swath.
-    :param file_path: The original filepath of the swath, used for naming
-    :param tile_size: the size of the tile for sampling.
-    :return: a list of two arrays: a 4-d payload array of sample,channel.w*h; and a metadata array of slice (w & h)
-    This script will randomly sample down the swatch array that it inherits. Excluding padding, which varies depending
-    on the tile size, the code iterates down the swath by pixel, randomly across the breadth of the swath for a tile.
-    The mid-point of the swath where we expect labelled data in the future has been purposely avoided in the sampling.
-    In addition to passing back sliced tiles across all channels, the script also returns the slice values themselves
-    as metadata for future visualisation
-    """
-    _, tail = os.path.split(file_path)
-
-    nullcheck = (lambda x: np.isnan(x).any())
-
-    swath_bands, swath_length, swath_breadth = swath_array.shape
-
-    filecheck_nans = nullcheck(swath_array)
-    if filecheck_nans:
-        print("WARNING: {} nan check failed".format(tail))
-
-        non_nan_in_array = np.count_nonzero(~np.isnan(swath_array))
-        elements_in_array = len(swath_array.flatten())
-
-        print("{} is {}% complete".format(tail, (non_nan_in_array/elements_in_array)*100))
+    mask = np.ones(mask_shape, dtype=np.uint8)
 
     offset = tile_size // 2
     offset_2 = offset
 
     if not tile_size % 2:
-        offset_2 = offset + 1
+        offset_2 -= 1
 
-    payload = []
-    metadata = []
+    # must not sample tile centers in the borders, so that tiles keep to required shape
+    mask[:, :offset] = 0
+    mask[:, -offset_2:] = 0
+    mask[:offset, :] = 0
+    mask[-offset_2:, :] = 0
 
-    for vertical_pixel in range((offset+1), swath_length-(offset+1)):
+    return mask
 
-        tiles_in_band = []
+def get_label_mask(class_channels):
+    """ given the class occurences channels, returns a 2d array marking as 1 the labelled pixels and as 0 the unlabelled ones."""
 
-        random_range = random.choice([(offset + 1, (swath_breadth//2)-(offset+1)),
-                                      ((swath_breadth//2)+(offset+1),  swath_breadth-(offset+1))])
+    labelmask = np.sum(class_channels, axis=0)
+    labelmask[labelmask > 0] = 1
 
-        random_horizontal_pixel = random.randint(*random_range)
+    return labelmask.astype(np.uint8) 
 
-        for band in range(swath_bands):
-
-            tile = swath_array[band,
-                               vertical_pixel - offset: vertical_pixel + offset_2 + 1,
-                               random_horizontal_pixel - offset: random_horizontal_pixel + offset_2 + 1
-                               ]
-
-            tiles_in_band.append(tile)
-
-        tile_metadata = [
-            (vertical_pixel - offset, vertical_pixel + offset_2 + 1),
-            (random_horizontal_pixel - offset, random_horizontal_pixel + offset_2 + 1)
-        ]
-
-        payload.append(tiles_in_band)
-        metadata.append(tile_metadata)
-
-    payload_array = np.stack(payload)
-
-    return[payload_array, metadata]
-
-
-def striding_tile_extract_from_file(swath_array, file_path, tile_size=3, stride=1):
+def sample_cloudy_unlabelled_tiles(swath_array, cloud_mask, label_mask, number_of_tiles, tile_size=3):
     """
-    :param swath_array: input numpy array from MODIS
-    :param file_path: the original filepath, for verbose functions
+    :param swath_array: input numpy array from MODIS of size (nb_channels, w, h)
+    :param cloud_mask: 2d array of zise (w, h) marking the cloudy pixels 
+    :param label_mask: 2d array of zise (w, h) marking the labelled pixels 
+    :param number_of_tiles: the number of tiles to sample. It is reset to maximal number of tiles that can be sampled, if bigger
     :param tile_size: size of the tile selected from within the image
-    :param stride: horizontal steps between images
-    :return: a list of two arrays: a 4-d payload array of sample,channel.w*h; and a metadata array of slice (w & h)
-
-    This script will systematically sample down the swatch array that it inherits. Excluding padding, which varies
-    depending on the tile size, the code iterates down the swath by pixel, and strides across the breadth of the
-    swath for a tile. The mid-point of the swath where we expect labelled data in the future has been purposely
-    avoided in the sampling. In addition to passing back sliced tiles across all channels, the script also returns
-    the slice values themselves as metadata for future visualisation
-
+    :return: a 4-d array (nb_tiles, nb_channels, w, h) of sampled tiles; and a list of tuples ((w1, w2), (h1, h2)) with the relative positions of the sampled tiles withing the swath
+    The script will use a cloud_mask channel to mask away all non-cloudy data and a label_mask channel to mask away all labelled data. The script will then randomly select a number of tiles (:param number of tiles) from the cloudy areas that are unlabeled.
     """
 
-    _, tail = os.path.split(file_path)
+    # mask not to sample outside the swath
+    allowed_pixels = get_sampling_mask(swath_array.shape[1:], tile_size)
 
-    nullcheck = (lambda x: np.isnan(x).any())
+    # combine the three masks, tile centers will be sampled from the cloudy and unlabelled pixels that are not in the borders of the swath
+    allowed_pixels = np.logical_and.reduce(allowed_pixels, cloud_mask, ~label_mask)
+    allowed_pixels_idx = np.where(allowed_pixels == 1)
+    allowed_pixels_idx = list(zip(*allowed_pixels_idx))
 
-    swath_bands, swath_length, swath_breadth = swath_array.shape
+    number_of_tiles = min(number_of_tiles, len(allowed_pixels_idx))
 
-    filecheck_nans = nullcheck(swath_array)
-    if filecheck_nans:
-        print("WARNING: {} nan check failed".format(tail))
-
-        non_nan_in_array = np.count_nonzero(~np.isnan(swath_array))
-        elements_in_array = len(swath_array.flatten())
-
-        print("{} is {}% complete".format(tail, (non_nan_in_array / elements_in_array) * 100))
-
-    _, tail = os.path.split(file_path)
-
-    nullcheck = (lambda x: np.isnan(x).any())
-
-    filecheck_nans = nullcheck(swath_array)
-    if filecheck_nans:
-        print("WARNING: {} nan check failed".format(tail))
-
-        non_nan_in_array = np.count_nonzero(~np.isnan(swath_array))
-        elements_in_array = len(swath_array.flatten())
-
-        print("{} is {}% complete".format(tail, (non_nan_in_array / elements_in_array) * 100))
-
-    offset = tile_size // 2
-    offset_2 = offset
-
-    if not tile_size % 2:
-        offset_2 = offset + 1
-
-    payload = []
-    metadata = []
-
-    lower_breadth_range = np.arange(start=(offset + 1),
-                                    stop=(swath_breadth // 2 - (offset + 1)),
-                                    step=stride)
-
-    upper_breadth_range = np.arange(start=(swath_breadth // 2 + (offset + 1)),
-                                    stop=(swath_breadth - (offset + 1)),
-                                    step=stride)
-
-    horizontal_pixels = np.append(lower_breadth_range, upper_breadth_range)
-
-    vertical_pixels = np.arange(start=(offset + 1),
-                                stop=(swath_length - (offset + 1)),
-                                step=stride)
-
-    centre_of_tile_position = []
-    for vertical_pixel in vertical_pixels:
-        for horizontal_pixel in horizontal_pixels:
-            coordinates = (vertical_pixel, horizontal_pixel)
-            centre_of_tile_position.append(coordinates)
-
-    for co_ord in centre_of_tile_position:
-        vertical_pos = co_ord[0]
-        horizontal_pos = co_ord[1]
-
-        bands_in_tile = []
-        for band in range(swath_bands):
-            tile = swath_array[band,
-                               vertical_pos - offset: vertical_pos + offset_2 + 1,
-                               horizontal_pos - offset: horizontal_pos + offset_2 + 1
-                               ]
-
-            bands_in_tile.append(tile)
-
-        tile_metadata = [
-            (vertical_pos - offset, vertical_pos + offset_2 + 1),
-            (horizontal_pos - offset, horizontal_pos + offset_2 + 1)]
-
-        metadata.append(tile_metadata)
-        payload.append(bands_in_tile)
-
-    payload_array = np.stack(payload)
-
-    return [payload_array, metadata]
-
-
-def extract_random_sample_where_clouds(swath_array, file_path, number_of_labels, tile_size=3, stride=3):
-    """
-    :param swath_array: input numpy array from MODIS
-    :param file_path: the original filepath, for verbose functions
-    :param number_of_labels: the number of labels selected - so that we select the amount of un-labelled tiles
-    :param tile_size: size of the tile selected from within the image
-    :param stride: horizontal steps between images
-    :return: a list of two arrays: a 4-d payload array of sample,channel.w*h; and a metadata array of slice (w & h)
-    The script will use a cloud_mask channel ([-2]) to mask away all non-cloudy data. The script will then randomly
-    select a number of tiles (:param number of labels) from the cloudy areas.
-    """
-
-    _, tail = os.path.split(file_path)
+    # sample without replacement
+    tile_centers = np.random.choice(allowed_pixels_idx, number_of_tiles, False)
     
     _, swath_length, swath_breadth = swath_array.shape
 
-    offset = tile_size // 2
-    offset_2 = offset
-
-    if not tile_size % 2:
-        offset_2 = offset + 1
-
-    payload = []
-    metadata = []
-
-    lower_breadth_range = np.arange(start=(offset + 1),
-                                    stop=(swath_breadth // 2 - (offset_2 + 1)),
-                                    step=stride)
-
-    upper_breadth_range = np.arange(start=(swath_breadth // 2 + (offset + 1)),
-                                    stop=(swath_breadth - (offset_2 + 1)),
-                                    step=stride)
-
-    horizontal_pixels = np.append(lower_breadth_range, upper_breadth_range)
-
-    vertical_pixels = np.arange(start=(offset + 1),
-                                stop=(swath_length - (offset_2 + 1)),
-                                step=stride)
-
-    masks = []
-    centre_of_tile_position = []
-    for vertical_pixel in vertical_pixels:
-        for horizontal_pixel in horizontal_pixels:
-            coordinates = (vertical_pixel, horizontal_pixel)
-            mask = swath_array[-9, vertical_pixel, horizontal_pixel]
-            masks.append(mask)
-            centre_of_tile_position.append(coordinates)
-
-    masks = np.array(masks, dtype=bool)
-    centre_of_tile_position = np.array(centre_of_tile_position)
-    masked_array = centre_of_tile_position[masks, :]
-
-    np.random.shuffle(masked_array)
-
-    chosen_random_tile_positions = masked_array[:number_of_labels]
-
-    for coord in chosen_random_tile_positions:
+    for coord in tile_centers:
         vertical_pos = coord[1]
         horizontal_pos = coord[0]
 
@@ -241,13 +73,12 @@ def extract_random_sample_where_clouds(swath_array, file_path, number_of_labels,
 
     payload_array = np.stack(payload)
 
-    return [payload_array, metadata]
+    return payload_array, metadata
 
 
-def extract_label_tiles(swath_array, file_path, tile_size=3):
+def extract_label_tiles(swath_array, cloud_mask, label_mask, tile_size=3):
     """
     :param swath_array: input swath, WITH labels as the last channel
-    :param file_path: name of the input swath - for providing context to errors
     :param tile_size: the size of the channels
     :return: nested list of extracted tile and metadata
     """
@@ -323,3 +154,33 @@ def extract_labels_and_cloud_tiles(swath_array, file_path, tile_size=3, stride=3
         stride=stride)
 
     return [labelled_payload, unlabelled_payload, labelled_metadata, unlabelled_metadata]
+
+
+if __name__ == "__main__":
+
+    mask = get_sampling_mask((3, 3), 3)
+    assert np.sum(mask) == 1
+
+    mask = get_sampling_mask((4, 4), 3)
+    assert np.sum(mask) == 4
+
+    mask = get_sampling_mask((4, 4), 4)
+    assert np.sum(mask) == 1
+
+    mask = get_sampling_mask((5, 5), 4)
+    assert np.sum(mask) == 4
+
+    labelmask = get_label_mask(np.ones((3, 5, 5)))
+    assert np.all(labelmask == 1)
+    assert labelmask.shape == (5, 5)
+
+    classes = np.zeros((3, 5, 6))
+    classes[0, 2:4, 1] = 3
+    classes[1, 1, 1] = 1
+    labelmask = get_label_mask(classes)
+
+    assert np.sum(labelmask) == 3
+    assert labelmask[2, 1] == 1
+    assert labelmask[3, 1] == 1
+    assert labelmask[1, 1] == 1
+    assert labelmask.shape == (5, 6)
