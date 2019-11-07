808895061e8ff18dca2270d48299eaf8f0aa1e70
Valentina Zantedeschi <vzantedeschi@gmail.com>
2019-11-07 14:58:39 +0100

    lower case

--- netcdf/npy_to_nc.py
+++ netcdf/npy_to_nc.py
@@ -7,27 +7,27 @@ import netCDF4 as nc4
 from src.track_alignment import map_and_reduce
 from src.utils import get_file_time_info, minutes_since    
 
-swath_channels = ['EV_250_Aggr1km_RefSB_1', 'EV_250_Aggr1km_RefSB_2', 'EV_1KM_Emissive_29', 'EV_1KM_Emissive_33', 'EV_1KM_Emissive_34', 'EV_1KM_Emissive_35', 'EV_1KM_Emissive_36', 'EV_1KM_RefSB_26', 'EV_1KM_Emissive_27', 'EV_1KM_Emissive_20', 'EV_1KM_Emissive_21', 'EV_1KM_Emissive_22', 'EV_1KM_Emissive_23', 'Latitude', 'Longitude', 'Cloud_Water_Path', 'Cloud_Optical_Thickness', 'Cloud_Effective_Radius', 'Cloud_Phase_Optical_Properties', 'cloud_top_pressure_1km', 'cloud_top_height_1km', 'cloud_top_temperature_1km', 'cloud_emissivity_1km', 'surface_temperature_1km', 'Cloud_Mask']
+swath_channels = ['ev_250_aggr1km_refsb_1', 'ev_250_aggr1km_refsb_2', 'ev_1km_emissive_29', 'ev_1km_emissive_33', 'ev_1km_emissive_34', 'ev_1km_emissive_35', 'ev_1km_emissive_36', 'ev_1km_refsb_26', 'ev_1km_emissive_27', 'ev_1km_emissive_20', 'ev_1km_emissive_21', 'ev_1km_emissive_22', 'ev_1km_emissive_23', 'latitude', 'longitude', 'cloud_water_path', 'cloud_optical_thickness', 'cloud_effective_radius', 'cloud_phase_optical_properties', 'cloud_top_pressure', 'cloud_top_height', 'cloud_top_temperature', 'cloud_emissivity', 'surface_temperature', 'cloud_mask']
 
 layer_info_channels = {
-    "type-layer" : 'Cloud_Layer_Type', 
-    "base-layer" : 'Cloud_Layer_Base',
-    "top-layer" : 'Cloud_Layer_Top', 
-    "type-quality" : 'Cloud_Type_Quality',
-    "precip-flag" : 'Precipitation_Flag',
+    "type-layer" : 'cloud_layer_type', 
+    "base-layer" : 'cloud_layer_base',
+    "top-layer" : 'cloud_layer_top', 
+    "type-quality" : 'cloud_type_quality',
+    "precip-flag" : 'precipitation_flag',
 }
 
 
 channel_params = { # offset, scale_factor, min_value, max_value
-    'Cloud_Water_Path' : [0., 1., 0, 10000], 
-    'Cloud_Optical_Thickness' : [0., 0.00999999977648258, 0, 10000], 
-    'Cloud_Effective_Radius' : [0., 0.00999999977648258, 0, 10000], 
-    'Cloud_Phase_Optical_Properties' : [0., 1., 0, 4], 
-    'cloud_top_pressure_1km' : [0., 0.100000001490116, 10, 11000], 
-    'cloud_top_height_1km' : [0., 1., 0, 18000], 
-    'cloud_top_temperature_1km' : [-15000, 0.00999999977648258, 0, 20000], 
-    'cloud_emissivity_1km' : [0., 0.00999999977648258, 0, 100], 
-    'surface_temperature_1km' : [-15000, 0.00999999977648258, 0, 20000], 
+    'cloud_water_path' : [0., 1., 0, 10000], 
+    'cloud_optical_thickness' : [0., 0.00999999977648258, 0, 10000], 
+    'cloud_effective_radius' : [0., 0.00999999977648258, 0, 10000], 
+    'cloud_phase_optical_properties' : [0., 1., 0, 4], 
+    'cloud_top_pressure' : [0., 0.100000001490116, 10, 11000], 
+    'cloud_top_height' : [0., 1., 0, 18000], 
+    'cloud_top_temperature' : [-15000, 0.00999999977648258, 0, 20000], 
+    'cloud_emissivity' : [0., 0.00999999977648258, 0, 100], 
+    'surface_temperature' : [-15000, 0.00999999977648258, 0, 20000], 
 }
 
 def copy_dataset(original_filename, copy_filename, deep=True):
@@ -43,7 +43,7 @@ def copy_dataset(original_filename, copy_filename, deep=True):
         if deep:
             for name, group in original.groups.items():
                 new_group = copy.createGroup(name)
-                block_list.append([new_group, group])
+                block_list.append((new_group, group))
 
         # copy global attributes
         copy.setncatts({a : original.getncattr(a) for a in original.ncattrs()})
@@ -77,7 +77,7 @@ def fill_dataset(dataset, variables, swath, layer_info, minutes, status="dayligh
             invalid_mask = (swath[i] < channel_params[channel][2]) | (swath[i] > channel_params[channel][3])
 
             # scale values
-            swath[i] = (swath[i] + channel_params[channel][0]) * channel_params[channel][1]
+            swath[i] = (swath[i] - channel_params[channel][0]) * channel_params[channel][1]
 
             # fill values outside valid range with fillvalue            
             swath[i][invalid_mask] = variables[channel]._FillValue
@@ -104,14 +104,14 @@ def fill_dataset(dataset, variables, swath, layer_info, minutes, status="dayligh
 
             # correct values, from [1, 8] to [0, 7]
             if info_name == "type-layer":
-                info[info > -1] -= 1
+                info -= 1
                 info[info < 0] = variables[channel]._FillValue
 
         variables[channel][0] = info
 
     # set global variables and attributes
     dataset.status_flag = status
-    dataset["Time"][0] = minutes
+    dataset["time"][0] = minutes
 
 def load_npys(swath_path, layer_info_dir="layer-info"):
 
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     # get time info
     year, abs_day, hour, minute = get_file_time_info(swath_path)
 
-    # create a copy of reference dataset
+    #create a copy of reference dataset
     copy_name = "A{}.{}.{}{}.nc".format(year, abs_day, hour, minute)
     copy, variables = copy_dataset("netcdf/cumulo.nc", copy_name)
 
