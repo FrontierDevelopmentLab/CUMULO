258c481f3111ef18a262ef074cff2c9e675e2985
Valentina Zantedeschi <valentina.zantedeschi@univ-st-etienne.fr>
2020-04-30 16:30:34 +0200

    remove outdated scripts

--- run_pipeline.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-month="05";
-year="2009";
-find "/mnt/modisaqua/$year/MODIS/data/MYD021KM/collection61/$year/$month/" -type f | grep "MYD021KM" | xargs --max-procs=10 -n 1 python -W ignore pipeline.py "../disks/disk1/$year/$month/"
-# cat "missing$month.txt" | xargs --max-procs=10 -n 1 python -W ignore pipeline.py "../disks/disk2/$year/$month/"
