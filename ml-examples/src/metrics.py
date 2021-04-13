abc3623399015847ec4ab445ec60c1f80076ac56
Valentina Zantedeschi <vzantedeschi@gmail.com>
2021-04-13 15:52:52 +0100

    add sketch of physical evaluation

--- ml-examples/src/metrics.py
+++ ml-examples/src/metrics.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from src.loader import get_most_frequent_label
+
 def get_tp_tn_fp_fn(conf_matrix):
 
     TP = np.diag(conf_matrix)
@@ -24,3 +26,31 @@ def scores_per_class(conf_matrix):
     accuracy = (TP + TN) / (TP + FP + FN + TN)
 
     return np.nan_to_num(accuracy), np.nan_to_num(f1)
+
+def hist1D_per_class(loader, bins, nb_classes=8):
+    
+    histograms = {"target": np.zeros((nb_classes, len(bins) - 1)), "pred": np.zeros((nb_classes, len(bins) - 1))}
+    counts = {"target": np.zeros((nb_classes, 1)), "pred": np.zeros((nb_classes, 1))}
+
+    for properties, rois, labels, predictions in loader():
+
+        # get values only for pixels that have valid property values, are cloudy
+        mask = np.logical_and(~properties.mask, rois)
+        
+        # get target only for pixels with identified cloud types
+        target_valid = np.logical_and(mask, labels.sum(-1) > -10)
+
+        target = get_most_frequent_label(labels[target_valid])
+
+        for cl in range(nb_classes):
+            
+            target_values = properties.data[target_valid][target == cl]
+            pred_values = properties.data[mask][predictions[mask] == cl]
+
+            counts["target"][cl] += target_values.size
+            histograms["target"][cl] += np.histogram(target_values, bins=bins, density=False)[0]
+
+            counts["pred"][cl] += pred_values.size
+            histograms["pred"][cl] += np.histogram(pred_values, bins=bins, density=False)[0]
+    
+    return histograms, counts
